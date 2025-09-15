a77bc7a1ff27bec2fcb1a215a9302b763daf1244
Nakshatra Rane <nakshatrarane503@gmail.com>
2025-09-15 11:46:09 +0530

    Add files via upload

---
+++ Capstone Project 1/postgreDB.sql
@@ -0,0 +1,7 @@
+CREATE USER retailuser WITH PASSWORD 'yourpasswor';
+
+GRANT ALL PRIVILEGES ON DATABASE retaildb TO retailuser;
+
+GRANT ALL PRIVILEGES ON SCHEMA public TO retailuser;
+
+Select * from sales;
